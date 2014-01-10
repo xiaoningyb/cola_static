9c1190437ed30b5bc198494063eefb713f2fe74a
xiaoningyb <xiaoningyb@gmail.com>
2014-01-10 11:48:31 +0800

    added new js

--- icson/cola.mobile.js
+++ icson/cola.mobile.js
@@ -4,9 +4,12 @@
  */
 var cola={};
 var domain=location.protocol+'//'+location.host+'/'+(/weixin\.yixun\.com/.test(location.host.toString())?'coladev':'cola');
+//domain='http://3pm.event.51buy.com/cola';
 var timeFlag=1;
 var COOKIE_N_PRDID='cola_prdid';
 var COOKIE_N_ADRID='cola_addrid';
+var lotteryId=245;
+var actId=17091;
 var codeMapping={
 		'101':'请求失败',
 		'102':'OAuth错误',
@@ -14,6 +17,7 @@ var codeMapping={
 		'104':'易迅登录认证失败',
 		'105':'OAuth登录认证失败',
 		'110':'该瓶盖码已被使用过',
+		'201':'未知错误',
 		'10001':'CMEM调用错误',
 		'10002':'该用户不存在',
 		'20001':'您已经预约过啦',
@@ -84,27 +88,70 @@ cola.initTimer=function(callback){
 };
 
 
-
-
 /**
  * 兑换商品初始化
  */
 cola.InitGift=function(){
-	this._getGiftList=function(){
-
-	};
-
-	this._bindEvent=function(){
-
-	};
-
-	this._exchangeGift=function(){
-		var url=domain+'/exchange/trade?';
-		var params={'product':prdId};
+	this.getGiftList=function(callback,groupId){
+		var url=domain+'/exchangequery?callback=?';
+		var params={};
+		if(typeof groupId != 'undefined'){
+			params.type=groupId;
+		}
 		$.getJSON(url,params,function(rp){
-
+			if(rp.errno==0){
+				callback(rp);
+			}
+			else{
+				console.log('exchangequery error,errno:'+rp.errno);
+			}
 		});
 	};
+
+	this.exchangeGift=function(prdId,callback){
+		var url=domain+'/exchangetrade?callback=?';
+		var params={'productId':prdId};
+		$.getJSON(url,params,function(rp){
+			callback(rp);
+		});
+	};
+
+	this.getGiftInfo=function(prdId,callback){
+		var url=domain+'/exchangedetail?callback=?';
+		var params={'productId':prdId};
+		$.getJSON(url,params,function(rp){
+			if(typeof callback == 'function'){
+				callback(rp);
+			}
+		});
+	};
+
+	this.getUserAddress=function(callback){
+		var url=domain+'/getaddr?callback=?';
+		$.getJSON(url,function(rp){
+			if(rp.errno==0){
+				callback(rp);
+			}
+			else{
+				alert(cola.code2error(rp.errno));	
+			}
+		});
+	};
+
+	this.addUserAddress=function(strAddress,callback){
+		var url=domain+'/setaddr?callback=?';
+		$.getJSON(url,function(rp){
+			if(rp.errno==0){
+				if(typeof callback == 'function'){
+					callback(rp);
+				}
+			}
+			else{
+				alert(cola.code2error(rp.errno));	
+			}
+		});
+	};
+	return this;
 };
 
 /**
@@ -145,51 +192,51 @@ cola.initSk=function(){
 				prdInfo.stock=itemInfo.stock;
 				prdInfo.stockAll=itemInfo.stockAll;
 				prdInfo.price=itemInfo.qiangPrice;
-				prdInfo.yixunprice=itemInfo.yixunPrice;
+			        prdInfo.yixunprice=itemInfo.yixunPrice;
 				self._showProduct(idx,prdInfo);
 			}
 		});
 	};
-	
-	this._showProduct=function(idx,data){
+
+        this._showProduct=function(idx,data){
 	    var skId='sk_'+idx+'_bt';
-		var html=[];
-        html.push('<a href="#" class="ms_goods1_img">');
-        html.push('<img src="'+data.pic+'" alt="">');
-        if(idx==0) {
+	    var html=[];
+            html.push('<a href="#" class="ms_goods1_img">');
+            html.push('<img src="'+data.pic+'" alt="">');
+            if(idx==0) {
         	html.push('<span class="ms_goods1_tips1">0元秒</span></a>');
-        }
-        else {
+            }
+            else {
         	html.push('<span class="ms_goods1_tips2">3折秒</span></a>');
-        }
-                
-		html.push('<div class="ms_goods1_info">');
-        html.push('<a class="ms_goods1_name" href="#">'+data.title+'</a>');
-        html.push('<div class="ms_goods1_price"><span class="price">&yen;'+parseFloat(data.price)
-        		  +'</span> <del class="old_price">&yen;'+parseFloat(data.yixunprice)+'</del></div>');
-        html.push('<div class="ms_goods1_kc">');
-        html.push('<span class="ms_goods1_kc_txt">库存</span>');
-        html.push('<span class="ms_goods1_kc_show"><span style="width:'+(data.stock*100/data.stockAll)+'%"></span></span></div>')
-        html.push('<div class="ms_goods1_opt">');
-        if(timeFlag>0){
+            }
+            
+	    html.push('<div class="ms_goods1_info">');
+            html.push('<a class="ms_goods1_name" href="#">'+data.title+'</a>');
+            html.push('<div class="ms_goods1_price"><span class="price">&yen;'+parseFloat(data.price)
+        	      +'</span> <del class="old_price">&yen;'+parseFloat(data.yixunprice)+'</del></div>');
+            html.push('<div class="ms_goods1_kc">');
+            html.push('<span class="ms_goods1_kc_txt">库存</span>');
+            html.push('<span class="ms_goods1_kc_show"><span style="width:'+(data.stock*100/data.stockAll)+'%"></span></span></div>')
+            html.push('<div class="ms_goods1_opt">');
+            if(timeFlag>0){
         	html.push('<a class="ms_btn_qiang" id="'+skId+'" href="javascript:;">3点开抢</a>');
-        }
-        else {
+            }
+            else {
         	html.push('<a class="ms_btn_qiang" id="'+skId+'" href="javascript:;">立即秒杀</a>');
-        }
-        html.push('</div></div>');
-        $('.ms_goods1').eq(idx).html(html.join(''));
-		var self=this;
-		$('#'+skId).click(function(){
-			if(timeFlag>0){
-				alert('请耐心等待，秒杀将于今日15:00准时开始哦!');
-				return false
-			};
-			var url='seckillorder?prdId='+data.prdId;
-			self._doSk(data.prdId,url);
-			return false;
-		});
-	};
+            }
+            html.push('</div></div>');
+            $('.ms_goods1').eq(idx).html(html.join(''));
+	    var self=this;
+	    $('#'+skId).click(function(){
+		if(timeFlag>0){
+		    alert('请耐心等待，秒杀将于今日15:00准时开始哦!');
+		    return false
+		};
+		var url='seckillorder?prdId='+data.prdId;
+		self._doSk(data.prdId,url);
+		return false;
+	    });
+	};	
 
 	this._doSk=function(prdId,url){
 		if(cola.isLogin()){
@@ -232,34 +279,30 @@ cola.initSuperBuy=function(callback){
 
 /**
  *  用户抽奖初始化
- * @param  {[type]} callbackRender 奖项展现回调方法
- * @param  {[type]} callbackShow   抽奖时的展现
  * @return {[type]}                [description]
  */
-cola.initLottery=function(callbackRender,callbackShow){
-	var self=this;
-	(function(){
-		var url=domain+'/lotteryquery?component_id=1224&callback=?';
+cola.initLottery=function(){
+	this.getLotteryList=function(callback){
+		var url=domain+'/lotteryquery?component_id='+lotteryId+'&callback=?';
 		$.getJSON(url,function(rp){
 			if(rp.errno==0){
-				if(typeof callbackRender == 'function'){
-					callbackRender(rp.award_list);
-					self.runLottery();
+				if(typeof callback == 'function'){
+					callback(rp.award_list);
 				}
-				
 			}
 		});
-	})();
-	
-	this.runLottery=function(){
-		$('#bt_lottery').click(function(){
-			var url=domain+'/lottery?component_id=1224&verify_code=1&callback=?';
-			$.getJSON(url,function(rp){
-				callbackShow(rp);
-			});
-		});
-		
 	};
+	
+	this.getLottery=function(callback){
+		var url=domain+'/lottery?component_id='+lotteryId+'&verify_code=1&callback=?';
+		$.getJSON(url,function(rp){
+			if(typeof callback == 'function'){
+				callback(rp);
+			}
+		});
+	};
+
+	return this;
 };
 
 
@@ -270,12 +313,12 @@ cola.initLottery=function(callbackRender,callbackShow){
  */
 cola.getDiscountList=function(callback){
 	var settings={
-		url: 'http://event.yixun.com/event/17091_info.js',
+		url: 'http://event.yixun.com/event/'+actId+'_info.js',
 		dataType: 'script',
 		scriptCharset: 'gbk',
 		success:function(){
 			if(typeof callback == 'function'){
-				callback.apply(this,GoodsInfo_51buy.pblock[0].list);
+				callback(GoodsInfo_51buy.pblock[0].list);
 			}
 		}
 	};
@@ -297,43 +340,6 @@ cola.getUserAddress=function(callback){
 		}
 		else{
 			alert('地址信息拉取失败');
-		}
-	});
-};
-
-/**
- * 用户存储可乐币
- * @param  {[type]} pincode [description]
- * @return {[type]}         [description]
- */
-cola.storePincode=function(pincode){
-	var url=domain+'/colacoinadd?callback=?';
-	var params={'pincode':pincode};
-	$.getJSON(url,params,function(rp){
-		if(rp.errno==0){
-
-		}
-		else{
-			console.log('error:'+rp.errno);
-		}
-	});
-};
-
-/**
- * 获取可乐币数量
- * @param  {Function} callback [description]
- * @return {[type]}            [description]
- */
-cola.getPincodeCount=function(callback){
-	var url=domain+'/colacoinquery?callback=?';
-	$.getJSON(url,function(rp){
-		if(rp.errno==0){
-			if(typeof callback == 'function'){
-				callback.apply(this,rp.data.colacoin);
-			}
-		}
-		else{
-			console.log('error:'+rp.errno);
 		}
 	});
 };
@@ -368,15 +374,18 @@ cola.getProductInfo=function(prdId,callback){
 
 /**
  * [storePincode description]
- * @param  {[type]} pincode [description]
- * @return {[type]}         [description]
+ * @param  {[type]}   pincode  [description]
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
  */
-cola.storePincode=function(pincode){
+cola.storePincode=function(pincode,callback){
 	var url=domain+'/cola/colacoinadd?callback=?';
 	var params={'pincode':pincode};
 	$.getJSON(url,params,function(rp){
 		if(rp.errno==0){
-			cola.getPincodeCount();
+			if(typeof callback == 'function'){
+				callback();
+			}
 		}
 		else{
 			alert(cola.code2error(rp.errno));
@@ -388,14 +397,17 @@ cola.storePincode=function(pincode){
  * 查询用户可用的可乐币数量
  * @return {[type]} [description]
  */
-cola.getPincodeCount=function(){
-	var url=domain+'/cola/colacoinquery?callback=?';
+cola.getPincodeCount=function(callback){
+	var url=domain+'/colacoinquery?callback=?';
 	$.getJSON(url,function(rp){
+		callback(rp.data.colacoin);
 		if(rp.errno==0){
-			$('#pincode_count').html(rp.data.colacoin);
+			if(typeof callback == 'function'){
+				callback(rp.data.colacoin);
+			}
 		}
 		else{
-			console.log('get pincode errno:'+rp.errno);
+			console.log('error:'+rp.errno);
 		}
 	});
 };
@@ -429,7 +441,7 @@ cola.skSubscribe=function(){
 	if(cola.isLogin()){
 		var url=domain+'/SeckillSubscribe?callback=?';
 		$.getJSON(url,function(rp){
-			if(rp.errno==0){
+		        if(rp.errno==0){
 				$('#tips').show();
 				$('#sk_no').hide();
 				$('#sk_join').show();
@@ -453,8 +465,8 @@ cola.skSubscribe=function(){
 				$('#sk_join').hide();
 			}
 			else{
-			    $('#tips1_txt').html('<p>系统错误:'+rp.errno+'</p>');
-				$('#tips1').show();
+			        $('#tips1_txt').html('<p>系统错误:'+rp.errno+'</p>');
+			        $('#tips1').show();
 			}
 		});
 	}
@@ -493,7 +505,41 @@ cola.bindQQ=function(qq){
 	});
 };
 
+/**
+ * [getDate description]
+ * @return {[type]} [description]
+ */
+cola.getDate=function(){
+	this._numMap={
+		'1':'一',
+		'2':'二',
+		'3':'三',
+		'4':'四',
+		'5':'五',
+		'6':'六',
+		'7':'七',
+		'8':'八',
+		'9':'九',
+		'10':'十',
+		'11':'十一',
+		'11':'十二'
+	};
+	this._convertMonth=function(num){
+		return typeof this._numMap[num]=='undefined'?'':this._numMap[num];
+	};
 
+	var d=new Date();
+	var rt={
+		'year':d.getFullYear(),
+		'month':d.getMonth()+1,
+		'cn_month':this._convertMonth(d.getMonth()+1),
+		'date':d.getDate(),
+		'hour':d.getHours(),
+		'minute':d.getMinutes(),
+		'second':d.getSeconds()
+	};
+	return rt;
+};
 
 /**
  * 从服务器端获取距离活动的开始时间
@@ -560,7 +606,7 @@ cola.isLogin=function(){
 };
 
 cola.code2error=function(errorCode){
-	return codeMapping[errorCode];
+	return typeof codeMapping[errorCode]=='undefined'?errorCode:codeMapping[errorCode];
 };
 
 /**
@@ -572,14 +618,6 @@ cola.getParam=function(name){
 	var reg=new RegExp(name+'=([^&]*)');
 	var m=location.href.match(reg);
 	return !m?'':m[1];
-};
-
-/**
- * 列表页的加载
- * @return {[type]} [description]
- */
-cola.listLazyLoad=function(){
-	$("img.lazy").lazyload();
 };
 
 
@@ -622,6 +660,11 @@ cola.checkPincode=function(pincode){
 	var reg=new RegExp('^[a-zA-Z0-9]{13}$');
 	return reg.test(pincode);
 };
+
+cola.goback=function(url){
+	var url=url||document.referrer
+	window.location=url;
+}
 
 
 
