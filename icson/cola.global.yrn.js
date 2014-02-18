23c8cb8489936099d53d3293694d247b37ce9438
xiaoningyb <xiaoningyb@gmail.com>
2014-02-18 15:27:21 +0800

    added the new js

---
+++ icson/cola.global.yrn.js
@@ -0,0 +1,311 @@
+var colaGlobal=function(){
+	var self=this;
+
+	this.showFooter=function(){
+		var html=[];
+		html.push('<!-- 精选品牌活动 开始 -->');
+		html.push('<div class="act_hot">');
+		html.push('<div class="act_hot_hd"><h3>精选品牌热门活动</h3></div>');
+		html.push('<div class="act_hot_bd">');
+		html.push('<ul class="clear">');
+		html.push('<li><a href="#"><img src="http://ecd.oa.com/240x120" alt="图片名称" /></a></li>');
+		html.push('<li><a href="#"><img src="http://ecd.oa.com/240x120" alt="图片名称" /></a></li>');
+		html.push('<li><a href="#"><img src="http://ecd.oa.com/240x120" alt="图片名称" /></a></li>');
+		html.push('<li><a href="#"><img src="http://ecd.oa.com/240x120" alt="图片名称" /></a></li>');
+		html.push('</ul>');
+		html.push('</div>');
+		html.push('</div>');
+		html.push('<!-- 精选品牌活动 结束 -->');
+		html.push('<!-- 感谢合作方提供部分秒杀及兑换商品 开始 -->');
+		html.push('<div class="act_hzf">');
+		html.push('<div class="act_hzf_hd"><h3>感谢合作方提供部分秒杀商品</h3></div>');
+		html.push('<div class="act_hzf_bd">');
+		html.push('<ul class="clear">');
+		html.push('<li><a href="#"><img src="http://ecd.oa.com/120x60" alt="图片名称" /></a></li>');
+		html.push('<li><a href="#"><img src="http://ecd.oa.com/120x60" alt="图片名称" /></a></li>');
+		html.push('<li><a href="#"><img src="http://ecd.oa.com/120x60" alt="图片名称" /></a></li>');
+		html.push('<li><a href="#"><img src="http://ecd.oa.com/120x60" alt="图片名称" /></a></li>');
+		html.push('<li><a href="#"><img src="http://ecd.oa.com/120x60" alt="图片名称" /></a></li>');
+		html.push('<li><a href="#"><img src="http://ecd.oa.com/120x60" alt="图片名称" /></a></li>');
+		html.push('<li><a href="#"><img src="http://ecd.oa.com/120x60" alt="图片名称" /></a></li>');
+		html.push('<li><a href="#"><img src="http://ecd.oa.com/120x60" alt="图片名称" /></a></li>');
+		html.push('</ul>');
+		html.push('</div>');
+		html.push('</div>');
+		html.push('<!-- 感谢合作方提供部分秒杀及兑换商品 结束 -->');
+		html.push('<!-- 活动规则 开始 -->');
+		html.push('<div class="act_rule f_tx1">');
+		html.push('<div class="act_rule_hd"><h3>活动规则</h3></div>');
+		html.push('<div class="act_rule_bd">');
+		html.push('<ul>');
+		html.push('<li>一、活动时间：</li>');
+		html.push('<li>2014年3月3日至2014年5月31日；</li>');
+		html.push('<li>二、参与方式：</li>');
+		html.push('<li>活动期间，凭“午后畅爽秒大奖”可口可乐公司促销产品瓶盖内的13位瓶盖码，登录易迅网活动页3pm.yixun.com，或关注可口可乐官方微信，即可参与活动；</li>');
+		html.push('<li>三、秒杀参与指引：</li>');
+		html.push('<li>1. 秒杀时间：活动期间，每天下午3点开启，24点结束。</li>');
+		html.push('<li>2. 入场资格：活动期间，每天0点至24点间，可用1个瓶盖码兑换当天秒杀场的秒杀资格。秒杀资格兑换仅限当天场次，其他日期场次的秒杀资格不能提前兑换。</li>');
+		html.push('<li>3. 秒杀商品：任意秒杀商品，均为先到先得，秒完即止。包括：每天300台可口可乐定制版0元三星手机，爆款3折起秒杀商品及易迅精选商品；具体秒杀商品及数量以当天实际活动为准。0元三星手机、爆款3折起秒杀商品、部分易迅精选商品，每个id限购1件。</li>');
+		html.push('<li>4. 易迅网5元优惠券：用瓶盖码兑换秒杀资格时，可同时获赠一张易迅网5元优惠券。优惠券仅限本人使用，有效期为领取之日起90天内，易迅网全网通用（虚拟及特价商品除外），详细使用说明请查看优惠券介绍。（点击后打开新页面：http://st.yixun.com/help/1-4-coupon.htm）</li>');
+		html.push('<li>5. 瓶盖码存储：多余的瓶盖码可先在活动页面进行存储，兑换秒杀资格时可以直接消耗已存储的瓶盖码。</li>');
+		html.push('<li>6. 页面内商品售完即作下架及更新处理，最新商品信息请用户及时刷新活动页面。</li>');
+		html.push('<li>7. 此促销活动只针对普通消费者，渠道经销商不享受优惠，一经发现活动主办方有权取消购买资格。</li>');
+		html.push('<li>8. 由于活动期间会短时间生成大量订单，所以部分订单配送可能会有所延缓，敬请谅解。</li>');
+		html.push('<li>9. 活动主办方拥有法律允许范围内的最终解释权。</li>');
+		html.push('<li>更多活动说明请点击查看详情</li>');
+		html.push('</ul>');
+		html.push('<p><a id="rules_more" href="javascript:;" class="lk_0">查看详情&gt;</a></p>');
+		html.push('</div>');
+		html.push('</div>');
+		html.push('<!-- 活动规则 结束 -->');
+		html.push('<!-- 二维码 -->');
+		html.push('<div class="act_ma">');
+		html.push('<a href="javascript:;">关闭</a>');
+		html.push('</div>');
+		return html.join('');
+	};
+
+	this.showWeibo=function(){
+		var self=this;
+		this.maxlen=140;
+		var options={
+			showFrame : "#zhuanfa_frame",
+			submitBtn : ".tencent_weibo_bt" ,
+			adsTitle : "可口可乐午后畅爽",
+			adsContent : "困乏的下午3点，你在刷眼皮？刷朋友圈？还是刷微博？刷不出人生high点，不如来瓶可乐激爽一下！现在开始搜集可口可乐瓶盖码，每天下午3点，300台0元三星手机等你秒！3月3日上易迅网3pm.yixun.com，惊喜正式开启！",
+			adsPic : "",
+			adsUrl : "http://www.yixun.com"
+		};
+
+		this._initTencentWeibo(options);
+		this.refreshNumber($('.input_text'),$('#ntotal'),self.maxlen);
+		$('#bt_sina').click(function(){
+			self._sinaWeibo($(".input_text").val(),options.adsUrl,options.adsPic);
+			if($('#chk_share').get(0).checked){
+				self._tencentWeibo(options.adsContent,options.adsUrl,options.adsPic);
+			}
+		});
+
+		$('.input_text').keyup(function(){
+			self.refreshNumber($(this),$('#ntotal'),self.maxlen);
+		});
+	};
+
+	this.refreshNumber=function(contentObj,showObj,maxlen){
+		var oval=contentObj.val();
+		var len=oval.length;
+		if(len>maxlen){
+			contentObj.val(oval.substr(0,maxlen));
+			len=maxlen;
+		}
+		showObj.text(len);
+	};
+
+	//新浪微博分享
+	this._sinaWeibo=function(title,url,pic){
+		var params={'title':encodeURIComponent(title),'url':encodeURIComponent(url),'pic':encodeURIComponent(pic)};
+		var _url="http://v.t.sina.com.cn/share/share.php?"+$.param(params);
+		window.open(_url,'_blank');
+	};
+
+	this._tencentWeibo=function(title,url,pic){
+		var params={
+			'title':title,
+			'url':encodeURIComponent(url),
+			'pic':encodeURIComponent(pic),
+			'appkey':801148568,
+			'site':'www.yixun.com'
+		};
+		var _url = 'http://v.t.qq.com/share/share.php?'+$.param(params);
+		window.open(_url, '', 'width=600,height=300,top=0,left=0,toolbar=no,menubar=no,scrollbars=no,location=no,resizable=no,status=no');
+	};
+
+	//腾讯微博分享
+	this._initTencentWeibo=function(options){
+		var self=this;
+		var o=options;
+		var fullAdsContent = "#" + o.adsTitle + "# " + o.adsContent;
+		$(".input_text").val(fullAdsContent + " ");
+		var frameUrl = "http://wall.v.t.qq.com/index.php?c=wall&a=index&t=" + encodeURIComponent(o.adsTitle) + "&ak=801138415&w=0&h=320&o=3&s=4&cs=545454_FFFFFF_CDFF9C_FFFFFF_007500_333333";
+		$(o.showFrame).attr("src", frameUrl);
+		$(o.submitBtn).click(function(){
+			self._tencentWeibo($(".input_text").val(),o.adsUrl,o.adsPic);
+		});
+	};
+
+	//获取用户当前可乐币数量
+	this.getPincodeCount=function(){
+		self.intervalFunction();	
+	};
+	
+	this.controlQrCode=function(){
+		$('div.act_ma >a').attr('title','关闭').click(function(){
+			$(this).parent().remove();
+		});
+	};
+
+	this.intervalFunction=function(){
+		cola.getPincodeCount(function(rp){
+			if(rp.errno==0){
+					self.incrNumber(rp.data.colacoin,'span.ycm_num');
+			}
+			else{
+				console.log('get pincode errno:'+rp.errno);
+			}
+		});
+	};
+
+	this.incrNumber=function(max,cnt){
+		var t=0;
+		var itvId=setInterval(function(){
+			if(t>=max){
+				clearInterval(itvId);
+				window.first=true;
+				return;
+			}
+			t++;
+			$(cnt).text(t);
+		},10);
+	};
+
+	this.showRules=function(){
+		var html=[];
+		html.push('<div class="mod_pop_bd" style="font-weight:normal;font-size:12px;height:450px;overflow-y:scroll">');
+		html.push('<div style="text-align:center"><h3> [可口可乐]“午后畅爽秒大奖”促销活动细则</h3></div>')
+		html.push('<p>一、<strong>活动介绍</strong></p>');
+		html.push('<p>　　2014年3月3日至2014年5月31日期间，符合本促销活动参加资格（详见第五条第2款）且拥有易迅网账户的消费者购买带有“午后畅爽秒大奖” 瓶盖促销标志的产品（“促销装产品”），用微信扫描促销装产品标签上的二维码或通过手机、电脑登录3pm.yixun.com （“活动网页”），并登录其个人易迅网账户，按要求输入促销装产品瓶盖内的13位码参与活动（“本促销活动”），可参加 “3点秒杀活动”一次，同时可获得“易迅网5元优惠券”一张。</p>');
+		html.push('<br>');
+		html.push('<p>二、<strong>活动主办方及活动区域</strong></p>');
+		html.push('<p>　　本促销活动由可口可乐(广西)饮料有限公司，可口可乐辽宁(南)饮料有限公司，可口可乐(湖北)饮料有限公司，可口可乐辽宁(北)饮料有限公司，可口可乐(黑龙江)饮料有限公司，可口可乐(山西)饮料有限公司，可口可乐(四川)饮料有限公司，可口可乐(重庆)饮料有限公司，可口可乐(云南)饮料有限公司，可口可乐(吉林)饮料有限公司，上海申美饮料食品有限公司，广东太古可口可乐有限公司，厦门太古可口可乐饮料有限公司，南京中萃食品有限公司，杭州中萃食品有限公司，西安中萃可口可乐饮料有限公司，郑州太古可口可乐饮料有限公司，合肥太古可口可乐饮料有限公司，天津可口可乐饮料有限公司，中粮可口可乐饮料（河北）有限公司，海南中粮可口可乐饮料有限公司，湖南中粮可口可乐饮料有限公司，甘肃中粮可口可乐饮料有限公司，湛江中粮可口可乐饮料有限公司，中粮可口可乐饮料（江西）有限公司，中粮可口可乐饮料（新疆）有限公司，中粮可口可乐饮料（山东）有限公司，中粮可口可乐饮料（济南）有限公司，北京可口可乐饮料有限公司，中粮可口可乐饮料（内蒙古）有限公司，珠海可口可乐饮料有限公司，上海易迅电子商务发展有限公司（“易迅网”）和三星（中国）投资有限公司（“三星公司”）联合主办（以下简称“活动主办方”）。</p>');
+		html.push('<p>　　为了让消费者更清楚地了解整个促销活动的过程和兑奖细则，活动主办方之一的可口可乐各地装瓶厂在促销活动的海报及促销包装上印有各地装瓶厂的促销活动咨询热线，消费者可以通过拨打该热线电话，咨询关于促销活动方式、奖品设置及中奖率、赢奖及兑奖方式等问题。</p>');
+		html.push('<br>');
+		html.push('<p>三、<strong>参加本促销活动的促销装产品</strong></p>');
+		html.push('<p>带有“午后畅爽秒大奖 ”瓶盖促销标志的以下品牌及规格的瓶装产品为参加本促销活动的产品：</p>');
+		html.push('<p><ul>');
+		html.push('<li>300毫升、500毫升、600毫升[可口可乐]汽水</li>');
+		html.push('<li>300毫升、500毫升、600毫升[雪碧]柠檬味汽水</li>');
+		html.push('<li>300毫升、500毫升、600毫升[芬达]橙味汽水</li>');
+		html.push('<li>300毫升、500毫升、600毫升[芬达]苹果味汽水</li>');
+		html.push('<li>300毫升、500毫升、600毫升[芬达]葡萄味汽水</li>');
+		html.push('<li>500毫升、600毫升[芬达]水蜜桃味汽水</li>');
+		html.push('</ul><p>');
+		html.push('<br>');
+		html.push('四、<strong>奖项设置及中奖率</strong>：');
+		html.push('</p>');
+		html.push('<p>');
+		html.push('<strong>参与“3点秒杀活动”</strong>:消费者需凭促销装产品瓶盖内的13位码登录活动网页，按提示操作可获得参与当天“3点秒杀活动” 一次的资格，并于当天下午3点起在活动网页参与“3点秒杀活动” （当天获得的活动资格仅可参加当天的“3点秒杀活动”），有机会“秒杀”获得价值人民币1000元的三星手机（ Samsung Galaxy Trend 3）一台 （份），先“秒”先得（以活动网站记录时间为准）。本促销活动期间“3点秒杀活动”每天提供300台（份）三星手机（Samsung Galaxy Trend 3），共计27000台（份），中奖率0.0027%。每个易迅网账户每天最多可“秒杀”获得一台三星手机。');
+		html.push('</p>');
+		html.push('<p>');
+		html.push('<strong>“易迅网5元优惠券”</strong>：消费者需凭促销装产品瓶盖内的13位码登录活动网页，按提示操作可获得“易迅网5元优惠券”一张（份）。凭此优惠券于指定有效期内在易迅网官方网站（http://www.yixun.com）购买商品（虚拟、特价产品除外），可享受人民币5元优惠一次。每笔订单限用一张此优惠券。本促销活动期间共设“易迅网5元优惠券”9.8544亿张（份），中奖率100%。');
+		html.push('</p>');
+		html.push('<p>注意事项：</br><ul>');
+		html.push('<li>该优惠券会以电子券形式发送至该消费者的易迅网账户中，其使用有效期为自发送至消费者易迅网账户之日（含当天）起90天，逾期无效。</li>');
+		html.push('<li>单笔订单只能使用一张该优惠券。该优惠券一旦使用，在订单生成后，将无法退回。如消费者取消其订单，订单中已使用的该优惠券将无法再次使用。</li>');
+		html.push('<li>如该优惠券金额大于订单金额，差额部分不予退回；如该优惠券金额小于订单金额，则需由消费者自行支付超出部分的费用；该优惠券不能用来支付相关运费；不可兑现，且不开发票。</li>');
+		html.push('<li>使用该优惠券支付的订单，如全部退款，按照实际支付金额退款；如部分退款，则按照退款部分所占订单总金额的比例计算所得相应的实际支付金额部分进行退款。优惠券金额不予退还。</li>');
+		html.push('<li>快递货到付款的商品，如果需要部分退货，则一律取消本订单的优惠券待遇且优惠券将无法再次使用。</li>');
+		html.push('<li>该优惠券只限于符合本促销活动参加资格的普通消费者使用，如发现有经销商使用优惠券，易迅网有权立即取消订单并做相关处理。</li>');
+		html.push('<li>易迅网客服电话：400-828-1878 (市话费自理,人工接听时间为:9:00-24:00)。</li>');
+		html.push('<li>易迅网可在法律允许范围内对此优惠券作出适当解释。</li>');
+		html.push('</ul></p>');
+		html.push('<br>');
+		html.push('<p>');
+		html.push('五、<strong>活动方式及兑奖</strong>');
+		html.push('</p>');
+		html.push('<p>');
+		html.push('1.2014年3月3日至2014年5月31日期间，符合本促销活动参加资格（详见第五条第2款）且拥有易迅网账户的消费者购买促销装产品，用微信扫描其标签上的二维码或通过手机、电脑登录3pm.yixun.com活动网页，并登录其个人易迅网账户，可在指定页面看到“立即兑换”操作按钮，点击“立即兑换”操作按钮，按要求输入促销装产品瓶盖内的13位码，则可获得参与当天“3点秒杀活动” 一次的资格，继而可参与当天下午3点开始的“3点秒杀活动”，同时可获得“易迅网5元优惠券”一张（份）。兑换完成后网页会提示“恭喜您获得今日3点秒杀资格及易迅网5元优惠券一张”。参与“3点秒杀活动”并成功“秒杀”获得三星手机的消费者，应按照网页提示正确填写寄送地址、姓名、联系电话等信息。易迅网将会以快递或邮寄的方式于活动网页系统成功收到消费者提交的上述有效寄送信息之日起30天内将其所获得的三星手机送至所填写的指定地址。');
+		html.push('</p>');
+		html.push('<p>');
+		html.push('2.本促销活动仅限不以营利为目的通过本促销活动的正规零售渠道直接购买促销装产品的消费者参加和兑奖。若不符合上述情形，则不具备参加本促销活动以及进一步兑奖的资格。主办方在其视为必要时保留要求兑奖者在兑换奖品前证明其符合上述兑奖资格的权利。');
+		html.push('</p>');
+		html.push('<p>');
+		html.push('3.如果发现有消费者使用任何不正当手段参加本促销活动，包括但不限于以下情况，活动主办方有权在不事先通知的前提下取消其参加活动的资格和获奖资格:');
+		html.push('</p>');
+		html.push('<p><ul>');
+		html.push('<li>a.任何有违本促销活动目的和宗旨；</li>');
+		html.push('<li>b.不遵守本促销活动规则；</li>');
+		html.push('<li>c.影响活动正常有序进行；</li>');
+		html.push('<li>d.损害活动主办方及相关实体的合法权益和声誉；</li>');
+		html.push('<li>e.在活动参与过程中提供非真实的数据或信息；</li>');
+		html.push('<li>f.以不正当、不公平或作弊等带有欺骗性质的手段参与活动；</li>');
+		html.push('<li>g.在参与活动过程中利用多个不同账户在本促销活动期间进行多次数或多账户的提交行为或其他虚假行为，或者在参与活动过程中进行虚假网络刷票行为，以虚假方式以求获得更多奖品。</li>');
+		html.push('<li>h.活动主办方有证据认为的其他不正当手段或情形。</li>');
+		html.push('</ul></p>');
+		html.push('<p>')
+		html.push('<br>');
+		html.push('六、<strong>兑奖截止时间</strong>')
+		html.push('</p>');
+		html.push('<p><ul>')
+		html.push('<li>1. 参与“3点秒杀活动”的截止时间为2014年5月31日24点。在 “3点秒杀活动”中成功“秒杀”获得三星手机后按照要求填写并提交正确的邮寄信息的截止时间为2014年6月1日凌晨1点。</li>');
+		html.push('<li>2. 兑换“易迅网5元优惠券”的截止时间为2014年5月31日24点。</li>');
+		html.push('<li>3. 若逾期不兑奖， 即作自动放弃处理， 主办方不予补偿。</li>');
+		html.push('</ul></p>');
+		html.push('<p>')
+		html.push('<br>');
+		html.push('七、<strong>本促销活动注意事项</strong>')
+		html.push('</p>');
+		html.push('<p><ul>')
+		html.push('<li>1. 主办方对兑奖者资格、促销瓶盖内信息、输入的13位码、系统回复的中奖短信的真实性和有效性拥有查验权和最终决定权。该13位码的输入须完整并符合活动的相关要求。促销瓶盖不得作价销售。兑奖者应符合上述第五条第2款的要求。任何不符合上述兑奖资格的兑奖者，一经发现，主办方有权取消其中奖资格和参加本促销活动的资格。任何伪造、变造、复制、作价销售的促销瓶盖及不符合相关要求的操作一律无效。促销瓶盖内的13位码仅能兑换一次“3点秒杀活动资格”及一张“易迅网5元优惠券”。</li>');
+		html.push('<li>2. 本促销活动中的易迅网5元优惠券由易迅网提供，如遇问题，请与易迅网联系解决（易迅网客服电话：400-828-1878 ，市话费自理，人工接听时间为：9:00-24:00）；本促销活动中的三星手机由三星公司提供，如遇任何产品质量或服务问题，请与三星公司或其指定的经销商上海普天太力通信科技公司联系（三星公司客服电话：400-810-5858，市话费自理，人工接听时间为：8:00-17:00），按照其正常的退换货、维修及服务规则办理相关事宜，其他活动主办方不承担责任。实物奖品以实物为准。</li>');
+		html.push('<li>3. 获奖者就本促销活动中所获得的奖品若按照法律规定须缴纳个人所得税，则该部分个人所得税由活动主办方承担。</li>');
+		html.push('<li>4. 如遇意外，无法提供指定奖品，主办方有权以价值相仿的奖品替代公布奖品。本促销活动中的奖品不得兑换现金、转让或作价销售，一经发现主办方有权取消其中奖资格。所有奖品金额均符合《中华人民共和国反不正当竞争法》的有关规定。</li>');
+		html.push('<li>5. 对于中奖者在领取和使用各奖品过程中发生的任何伤害和损失，如该等伤害和损失非由主办方之过错导致，主办方不承担任何责任。</li>');
+		html.push('<li>6. 在参与本促销活动发生的任何费用，包括但不限于网络通讯费、交通费或餐饮费等，均由消费者自行承担，活动主办方不予负责。</li>');
+		html.push('<li>7. 活动主办方、本促销活动的代理公司、广告公司、经销商、印刷商和相关合作企业（包括但不限于瓶盖供应商）之员工、家属，不可参加本促销活动, 以示公允。</li>');
+		html.push('<li>8. 本促销活动由上海市东方公证处公证。</li>');
+		html.push('<li>9. 消费者如对本促销活动规则有任何疑问，活动主办方将会在法律允许的范围内对本促销活动规则给予必要的说明和解释。如遇不可抗力因素，活动主办方拥有取消本促销活动的权利。</li>');
+		html.push('</ul></p>');
+		html.push('</div>');
+		var options={
+				'title':'活动规则',
+				'contents':html.join(''),
+				'okText':'关闭',
+				'width':800,
+				'height':600
+			};
+		$('#rules_more').click(function(){
+			cola.msgbox.show(null,null,options,4);
+		});
+	};
+
+	this.getLogoList=function(){
+		cola.getEventData(function(data){
+			if(data.adblock.length>0){
+				var result={'tp':['<ul class="clear">'],'bt':['<ul class="clear">']};
+				var row=null;
+				for(var i in data.adblock){
+					row=data.adblock[i];
+					if(/\-1$/.test(row.desc)){
+						result.tp.push('<li><a href="'+row.url+'"><img src="'+row.picUrl+'" alt="'+row.desc+'"/></a></li>');
+					}
+					else{
+						result.bt.push('<li><a href="'+row.url+'"><img src="'+row.picUrl+'" alt="'+row.desc+'"/></a></li>');
+					}
+				}
+				result.tp.push('</ul>');
+				result.bt.push('</ul>');
+				$('div.act_hot_bd').html(result.tp.join(''));
+				$('div.act_hzf_bd').html(result.bt.join(''));
+			}
+		});
+	};
+
+	this.run=function(){
+		var self=this;
+		$('div.act_cnt').html(this.showFooter());
+		this.controlQrCode();
+		this.getPincodeCount();
+		this.showWeibo();
+		this.showRules();
+		setTimeout(function(){
+			self.getLogoList();
+		},1000);
+		
+	};
+
+	this.run();
+};
+
+$(document).ready(function(){
+	new colaGlobal();
+});
